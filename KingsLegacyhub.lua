f0e72dcae72609e7a188c480aa9a8a397d523307
92yyry298ry2r <101843464+92yyry298ry2r@users.noreply.github.com>
2022-03-18 08:16:54 +0800

    Update KingsLegacyhub.lua

--- KingsLegacyhub.lua
+++ KingsLegacyhub.lua
@@ -1,8 +1,8 @@
 local PlaceId = game.PlaceId
-
+awdawfas
 if PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/92yyry298ry2r/Kings-Legacy/main/KingsLegacy1.lua")))()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/92yyry298ry2r/Kings-Legacy/main/KingsLegacy2.lua")))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/92yyry298ry2r/Kings-Legacy/main/KingsLegacy1.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/92yyry298ry2r/Kings-Legacy/main/KingsLegacy2.lua"))()
 else
     game.Players.LocalPlayer:kick("Only Execute this code if you are playing kings legacy")
 end
