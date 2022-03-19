127d9f6608c69e00718068ff41dc23dafd1a32d9
92yyry298ry2r <101843464+92yyry298ry2r@users.noreply.github.com>
2022-03-19 14:27:53 +0800

    Update KingsLegacyhub.lua

--- KingsLegacyhub.lua
+++ KingsLegacyhub.lua
@@ -1,5 +1,7 @@
 local PlaceId = game.PlaceId
 
+repeat wait() until game:IsLoaded()
+
 if PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/92yyry298ry2r/Kings-Legacy/main/KingsLegacy1.lua"))()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/92yyry298ry2r/Kings-Legacy/main/KingsLegacy2.lua"))()
